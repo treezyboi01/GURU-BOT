aab4141b27a15371876d40ac92d6f955814582b9
treezyboi01 <143810303+treezyboi01@users.noreply.github.com>
2023-11-30 06:02:57 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['15189364938, 'treezy', true],
+  ['15189364938', 'chitimbe', true], 
   [''] 
 ] //Number of owners
 
