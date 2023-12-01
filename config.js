c21c78968523f58fccf012717b04177385845946
treezyboi01 <143810303+treezyboi01@users.noreply.github.com>
2023-12-01 08:36:53 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['15189364938, 'treezy', true],
-  ['15189364938', 'chitimbe', true], 
+  ['26371437392, 'treezy', true],
+  ['26371437392', 'joker', true], 
   [''] 
 ] //Number of owners
 
@@ -50,10 +50,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
-global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.botname = 'joker bot'
+global.premium = 'false'
+global.packname = 'joker bot' 
+global.author = '@treezy' 
 global.menuvid = 'https://telegra.ph/file/085c4b1068f0f4f8db970.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
